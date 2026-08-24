8281d25c0a0f6b698904e121d091e7d16fc6d6e2
tattiii8 <t46.cloud@gmail.com>
2026-08-24 11:37:37 +0900

    deleuze-apiをダミーにする

--- deleuze-app/Program.cs
+++ deleuze-app/Program.cs
@@ -1,16 +1,7 @@
-using DeleuzeApp.Models;
-using DeleuzeApp.Services;
-using Microsoft.EntityFrameworkCore;
-
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddHttpContextAccessor(); // ITenantProvider で必要
-builder.Services.AddScoped<ITenantProvider, TenantProvider>();
-
-// DBコンテキストの登録
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // JWT認証の設定（省略せずに適切に設定してください）
 // builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)...
